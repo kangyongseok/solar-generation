fa27649fb8cd64c205e5a79db01e1726f84b81b1
Noah <kangyongsuek@gmail.com>
2023-11-14 22:24:07 +0900

    수정 배포 오류 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { TbSolarPanel2 } from "react-icons/tb";
 import RegisterForm from "./components/RegisterForm";
 
-export async function getData() {
+async function getData() {
   const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api`, {
     next: { revalidate: 10 },
   });
