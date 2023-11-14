f48da703a0a508dc6439d8074f0c96ecad0eb4c1
Noah <kangyongsuek@gmail.com>
2023-11-14 22:20:07 +0900

    init: 초기 프로젝트 세팅 및 설정

---
+++ src/app/api/route.ts
@@ -0,0 +1,43 @@
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import { JWT } from "google-auth-library";
+
+const SCOPES = [
+  "https://www.googleapis.com/auth/spreadsheets",
+  "https://www.googleapis.com/auth/drive.file",
+];
+
+const jwt = new JWT({
+  email: process.env.GOOGLE_CLIENT_EMAIL,
+  key: process.env.GOOGLE_PRIVATE_KEY,
+  scopes: SCOPES,
+});
+const doc = new GoogleSpreadsheet(
+  "1OWNg1l9xk_lQZpisxZEZxI8CfzC97QrbeCJmaop_Z7I",
+  jwt,
+);
+
+const getGoogleSheet = async () => {
+  await doc.loadInfo();
+  return doc;
+};
+
+const fetchGoogleSheetRows = async () => {
+  const googlesheet = await getGoogleSheet();
+  const sheeByIdElement = googlesheet.sheetsById[0];
+  const rows = await sheeByIdElement.getRows();
+  return rows.map((row) => ({
+    title: row.get("title"),
+    subTitle: row.get("subTitle"),
+  }));
+};
+
+export async function GET(request: Request) {
+  return Response.json(await fetchGoogleSheetRows());
+}
+
+export async function POST(request: Request) {
+  const res = await request.json();
+  const sheet = await doc.sheetsByIndex[1];
+  sheet.addRow(res);
+  return Response.json({});
+}
---
+++ src/app/components/RegisterForm/index.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+function RegisterForm() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const responst = await fetch(`${process.env.NEXT_PUBLIC_URL}/api`, {
+      method: "POST",
+      body: JSON.stringify({
+        name: e.target.name.value,
+        placeNumber: e.target.placeNumber.value,
+        phone: e.target.phone.value,
+      }),
+    });
+  };
+
+  return (
+    <section className="p-5">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-6">
+          <label htmlFor="name" className="mb-2 block text-sm font-medium">
+            이름
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+            placeholder="홍길동"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="placeNumber"
+            className="mb-2 block text-sm font-medium"
+          >
+            본인소유 건물 또는 토지지번
+          </label>
+          <input
+            type="text"
+            id="placeNumber"
+            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 "
+            required
+            placeholder="토지지번"
+          />
+        </div>
+        <div className="mb-6">
+          <label htmlFor="phone" className="mb-2 block text-sm font-medium">
+            연락처
+          </label>
+          <input
+            type="tel"
+            id="phone"
+            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 "
+            required
+            placeholder="010-0000-0000"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
+        >
+          제출하기
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default RegisterForm;
