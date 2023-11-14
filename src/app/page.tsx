dd599c09c7a704533532bc7bec344df5bf470bb0
Noah <kangyongsuek@gmail.com>
2023-11-14 23:23:37 +0900

    보완 배포

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 import { TbSolarPanel2 } from "react-icons/tb";
 import RegisterForm from "../components/RegisterForm";
 
-async function getData() {
-  const response = await fetch(`${process.env.PUBLIC_URL}/api`, {
-    next: { revalidate: 60 },
-  });
-  const data = await response.json();
-  return data;
-}
+// async function getData() {
+//   const response = await fetch(`${process.env.PUBLIC_URL}/api`, {
+//     next: { revalidate: 60 },
+//   });
+//   const data = await response.json();
+//   return data;
+// }
 
 export default async function Home() {
-  const data = await getData();
+  // const data = await getData();
 
   return (
     <div>
@@ -22,7 +22,7 @@ export default async function Home() {
           <span>Solar</span>
         </h1>
       </header>
-      {data.map(({ title, subTitle }: { title: string; subTitle: string }) => (
+      {/* {data.map(({ title, subTitle }: { title: string; subTitle: string }) => (
         <section key={title} className="h-screen break-keep pt-32 text-center">
           <h1 className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-4xl font-extrabold text-transparent sm:text-8xl">
             {title}
@@ -30,7 +30,7 @@ export default async function Home() {
             <span className="text-sky-500">{subTitle}</span>
           </h1>
         </section>
-      ))}
+      ))} */}
       <RegisterForm />
     </div>
   );
