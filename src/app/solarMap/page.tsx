ee87bc5bbb58598fc1059880fb677ed6796d4083
Noah <kangyongsuek@gmail.com>
2023-12-04 22:23:31 +0900

    add: 네이버 지도 적용

---
+++ src/app/solarMap/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Script from "next/script";
+import { useEffect, useRef } from "react";
+
+function SolarMap() {
+  const mapElement = useRef(null);
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!mapElement.current) return;
+
+    const location = new window.naver.maps.LatLng(37.5656, 126.9769);
+
+    const mapOptions = {
+      center: location,
+      zoom: 17,
+      zoomControl: true,
+    };
+
+    const map = new window.naver.maps.Map(mapElement.current, mapOptions);
+    // eslint-disable-next-line no-new
+    new window.naver.maps.Marker({
+      position: location,
+      map,
+    });
+  }, []);
+
+  return (
+    <>
+      <Script
+        strategy="beforeInteractive"
+        src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_MAP_KEY}`}
+      ></Script>
+      <div ref={mapElement} className="h-screen w-screen"></div>
+    </>
+  );
+}
+
+export default SolarMap;
